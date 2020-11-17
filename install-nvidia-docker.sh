3957132496a4843452ea85b7448c9d379650867e
frederictwc <frederictwc@live.com>
2020-11-17 17:56:25 +0800

    initial commmit

---
+++ install-nvidia-docker.sh
@@ -0,0 +1,11 @@
+curl https://get.docker.com | sh \
+  && sudo systemctl start docker \
+  && sudo systemctl enable docker
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+apt-get update
+apt-get install -y nvidia-docker2
+systemctl restart docker
